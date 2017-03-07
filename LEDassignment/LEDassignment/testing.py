ca9b1843c429a366ad798b225832d2d5205034f7
Thomas Flood <thomaspeterflood@gmail.com>
2017-03-07 17:07:30 +0000

    introduction of comments to 'testing' module

--- LEDassignment/LEDassignment/testing.py
+++ LEDassignment/LEDassignment/testing.py
@@ -3,6 +3,12 @@ Created on 7 Mar 2017
 
 @author: thomasflood
 '''
+
+# code for 'Introducting positional arguments'
+# as per Python documentation
+# the argparse module will be used in the 'main' function
+# it will act as interface between program and user
+
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("echo")
