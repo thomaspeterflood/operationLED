1dc3be86acdebc76533c23cfc63ccb2907c8e701
Thomas Flood <thomaspeterflood@gmail.com>
2017-03-07 16:53:46 +0000

    test utilisation of 'argparse' module

--- LEDassignment/LEDassignment/testing.py
+++ LEDassignment/LEDassignment/testing.py
@@ -3,12 +3,8 @@ Created on 7 Mar 2017
 
 @author: thomasflood
 '''
-def max(a, b):
-    if a > b:
-        return a
-    else: 
-        return b
-    
-print(max(15, 18))
-
-    
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("echo")
+args = parser.parse_args()
+print(args.echo)
